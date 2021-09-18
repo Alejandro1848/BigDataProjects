6b2bf393a546f71efc2ddf33a8f1872c70d6fe15
Alejandro Juárez Toribio <54814604+Alejandro1848@users.noreply.github.com>
2021-09-18 13:09:48 -0500

    Add files via upload

---
+++ san_francisco_bikeshare_trips.sql
@@ -0,0 +1,13 @@
+
+CREATE OR REPLACE TABLE `bike_insights.top_trips_2018_and_baty` AS
+SELECT
+    start_station_name,
+    COUNT(trip_id) as num_trips
+FROM
+    `bigquery-public-data.san_francisco_bikeshare.bikeshare_trips`
+WHERE start_date >'2017-12-31 00:00:00 UTC'
+GROUP BY
+    start_station_name
+ORDER BY num_trips DESC
+LIMIT
+    10
